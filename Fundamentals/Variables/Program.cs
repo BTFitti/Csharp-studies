4162bfc38cac4c039d7f6cfbfa213fd91bd8c203
BTFitti <bruno.fitti734@live.com>
2025-03-13 18:31:37 -0300

    Variables

---
+++ Fundamentals/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿// int
+// double
+// char 
+// string
+// bool
+
+// decimal
+// float
+// byte
+// sbyte
+// short
+// ushort
+// uint
+// long
+// ulong
+using System;
+using System.Globalization;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            double salary, height;
+            char gender;
+            string name;
+            age = 24;
+            salary = 4000.9;
+            height = 1.72;
+            gender = 'M';
+            name = "Robert";
+            Console.WriteLine($"Name: {name} \nAge: {age} \nGender: {gender} \nHeight: {height} \nSalary: {salary}");
+        }
+    }
+}
