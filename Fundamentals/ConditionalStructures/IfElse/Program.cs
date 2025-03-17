8570ad73006c667bdb8efa531d6f374fbb233839
BTFitti <bruno.fitti734@live.com>
2025-03-17 18:51:09 -0300

    learning C# conditional structures

---
+++ Fundamentals/ConditionalStructures/IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IfElse{
+    class Program{
+        static void Main(string[] args){
+            Console.Write("Digite a hora: ");
+            int hora = int.Parse(Console.ReadLine());
+            if(hora < 12){
+                Console.WriteLine("Bom dia!");
+            }else if(hora <= 18){
+                Console.WriteLine("Boa tarde!");
+            }else{
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
