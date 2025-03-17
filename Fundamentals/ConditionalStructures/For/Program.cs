2c82eecff5445cfa9af4085524a5a4ff50ff82be
BTFitti <bruno.fitti734@live.com>
2025-03-17 19:08:56 -0300

    Learning For C#

---
+++ Fundamentals/ConditionalStructures/For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, i, x, soma;
+            Console.Write("Quantos números serão digitados?");
+            N = int.Parse(Console.ReadLine());
+            soma = 0;
+            for (i = 1; i <= N; i++)
+            {
+                Console.Write("Digite um número: ");
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            System.Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
