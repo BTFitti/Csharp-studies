39d28d6e5a7b0837a5663646a29f5952e7c6217a
BTFitti <bruno.fitti734@live.com>
2025-03-17 19:14:43 -0300

    Learning Do While

---
+++ Fundamentals/ConditionalStructures/DoWhile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+using System.Globalization; 
+namespace DoWhile{
+    class Program{
+        static void Main(string[] args){
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            double C, F;
+            char resp;
+            do{
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CI);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CI));
+                Console.WriteLine("Deseja repetir (s/n)? ");
+                resp = char.Parse(Console.ReadLine());
+            }while(resp == 's' || resp == 'S');
+            if(resp == 'n' || resp == 'N'){
+                Console.WriteLine("Sua resposta foi: " + resp);
+                Console.WriteLine("Fim do programa!");
+            }
+        }
+    }
+}
