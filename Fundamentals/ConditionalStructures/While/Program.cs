723489c3fdfe759dabd1f835dac68896a440f1ea
BTFitti <bruno.fitti734@live.com>
2025-03-17 18:55:07 -0300

    Learning while in C#

---
+++ Fundamentals/ConditionalStructures/While/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace While{
+    class Program{
+        static void Main(string[] args){
+            Console.Write("Digite a hora: ");
+            int hora = int.Parse(Console.ReadLine());
+            while(hora < 12){
+                Console.WriteLine("Bom dia!: "+ hora+ "h");
+                hora++;
+            }
+        }
+    }
+}
